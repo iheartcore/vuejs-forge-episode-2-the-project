7d3a64f29269b2216c3953384a2d23a35fa29be7
Yo-Tina Verbraecken <64355830+iheartcore@users.noreply.github.com>
2022-08-31 16:22:31 +0200

    Excercise 6 - product reviews
    Adjusting .gitignore
    - Adding product reviews
    - Adding functionality so it gets saved on deskree.

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,17 @@
+
+<
+.env.example,d/4/d4dae00d11854b35292c2b8a30515a9a0aa2d871
+K
+requests/createProduct.http,f/9/f9bc49685fec9a0e0f26fe2e41167edfd365f688
+9
+	README.md,8/e/8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+H
+pages/products/index.vue,1/7/177d8dd1db6ae70cbbd4e804a505d61b41606526
+J
+components/ProductCard.vue,8/0/800db39ab7795308f2340cea3f6897c73c150fa9
+J
+components/ProductHeat.vue,4/1/4124f19bb1ed5d0dc9e9e52d9a4da742ee19a66e
+F
+server/api/products.js,f/2/f2231752990291ebf56fa0cf584181550d129ccb
+M
+components/ProductFilters.vue,f/8/f847a0b561065f3d408fa9b0360211664c5fad88
---
+++ components/ProductReviewsCard.vue
@@ -0,0 +1,39 @@
+<script setup>
+import { useDateFormat } from "@vueuse/core";
+const props = defineProps({
+  review: {
+    type: Object,
+    required: true,
+  },
+});
+const date = useDateFormat(new Date(props.review.createdAt), "MM-YYYY");
+</script>
+
+<template>
+
+
+  <div class="card border-2 border-gray-200 rounded-md">
+    <div class="card-body">
+      <div class="card-actions justify-between text-sm">
+          <div class="rating gap-1">
+            <input type="radio" name="rating-3" class="mask mask-heart bg-red-400" disabled :checked="review.rating === 1" />
+            <input type="radio" name="rating-3" class="mask mask-heart bg-orange-400" disabled :checked="review.rating === 2" />
+            <input type="radio" name="rating-3" class="mask mask-heart bg-yellow-400" disabled :checked="review.rating === 3" />
+            <input type="radio" name="rating-3" class="mask mask-heart bg-lime-400" disabled :checked="review.rating === 4" />
+            <input type="radio" name="rating-3" class="mask mask-heart bg-green-400" disabled :checked="review.rating === 5" />
+          </div>
+        <div>{{ date }}</div>
+      </div>
+      <h2 class="card-title">{{ review.title }}</h2>
+      <p>
+        {{ review.text }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .mask-heart{
+    cursor: initial;
+  }
+</style>
